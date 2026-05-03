dad68f7f76dc0f9bb73c1baf78d73f100f7079f9
Gizmo <me@aika.dev>
2026-05-03 12:05:05 +0800

    docs: add shadowBundle usage example

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -13,7 +13,8 @@ dependencies {
     runtimeOnly(libs.fabric.modmenu)
 }
 
-// shadow gradle example
+// The following code demonstrates how to use the Shadow Gradle plugin.
+// In most cases, you can safely remove it :)
 dependencies.implementation("net.objecthunter:exp4j:0.4.8").let { if (it != null) dependencies.shadowBundle(it) }
 tasks.shadowJar {
     relocate("net.objecthunter.exp4j", "${mod.group}.libs.exp4j")
